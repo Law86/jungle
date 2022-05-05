c8e2d9856e2306575f825d540ef1afe7ba725bd8
Michael Law <mikelaw86@hotmail.com>
2022-05-04 23:10:55 -0400

    Add counts for admin products/categories, and some admin login (on wrong branch)

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,9 @@
 class Admin::DashboardController < ApplicationController
+
+  http_basic_authenticate_with name: ENV["USER"], password: ENV["PASS"], if: -> { ENV["PASS"].present? }
+  
   def show
+    @products_total = Product.count
+    @category_total = Category.count
   end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,7 @@
 class Admin::ProductsController < ApplicationController
 
+  http_basic_authenticate_with name: ENV["USER"], password: ENV["PASS"], if: -> { ENV["PASS"].present? }
+
   def index
     @products = Product.order(id: :desc).all
   end
