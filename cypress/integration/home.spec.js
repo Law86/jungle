c3cfcc8489973663f1c2d3427600ddce6030b77e
Michael Law <mikelaw86@hotmail.com>
2022-05-11 14:05:18 -0400

    Add cypress tests

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,3 +1,17 @@
-it("There is products on the page", () => {
-  cy.get(".products article").should("be.visible");
+describe("Testing Home page", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("Display a title by default", () => {
+    cy.get("h1").should("contain", "Jungle");
+  });
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
 });
